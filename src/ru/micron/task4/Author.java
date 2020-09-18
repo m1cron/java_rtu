cd48e48980361964bd38202949d4ec87659f2dfa
m1cron <m1cron4900@gmail.com>
2020-09-18 18:39:19 +0300

    task4.2 release

---
+++ src/ru/micron/task4/Author.java
@@ -0,0 +1,37 @@
+package ru.micron.task4;
+
+public class Author {
+    private String name, email;
+    private char gender;
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                '}';
+    }
+}
